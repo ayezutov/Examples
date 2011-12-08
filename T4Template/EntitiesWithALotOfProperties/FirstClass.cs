4f23fb0e4c1285bc6c7d73bb23d5ffcd9db937d9
Alexander Yezutov <alexander.yezutov@gmail.com>
2011-12-08 10:51:46 +0300

    - Example was added

---
+++ T4Template/EntitiesWithALotOfProperties/FirstClass.cs
@@ -0,0 +1,9 @@
+﻿namespace T4Template.EntitiesWithALotOfProperties
+{
+    public partial class FirstClass
+    {
+         public void AccessSomeProperties()
+         {
+         }
+    }
+}
---
+++ T4Template/EntitiesWithALotOfProperties/FirstClass.generated.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace T4Template.EntitiesWithALotOfProperties
+{
+    public partial class FirstClass
+    {
+        [Category("Group1")] 
+        public string Property1 { get; set; }
+        [Category("Group1")] 
+        public DateTime Property2 { get; set; }
+        [Category("Group1")] 
+        public int Property3 { get; set; }
+
+        [Category("Group2")]
+        public string Property4 { get; set; }
+        [Category("Group2")]
+        public string Property5 { get; set; }
+    }
+}
